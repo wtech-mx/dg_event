4e8f5d6d66a9ae56ae1a652ef171f5129e8cddb0
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-06-20 18:25:54 -0500

    Contoles video

---
+++ assets/js/videobutons.js
@@ -0,0 +1,25 @@
+    var vid = document.getElementById("myVideo");
+
+    function playVid() {
+      vid.play();
+    }
+
+    function pauseVid() {
+      vid.pause();
+    }
+
+    function pauseplayVid() {
+            if (vid.paused) {
+                vid.play();
+            } else {
+                vid.pause();
+            }
+    }
+
+    function enableMute() {
+            if (vid.muted == true) {
+                vid.muted = false;
+            } else {
+                vid.muted = true;
+            }
+    }
