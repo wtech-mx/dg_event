fb2c2a03f53af2e04e05fc708a8dd609bb59aa93
verplancken <42614785+verplancken@users.noreply.github.com>
2022-06-22 21:42:47 -0500

    Revert "decoracion al dar pause"
    This reverts commit cee973df730d3048cf9636927fc215fd69bcb1b3.

--- assets/js/videobutons.js
+++ assets/js/videobutons.js
@@ -11,23 +11,16 @@
     function pauseplayVid() {
             if (vid.paused) {
                 vid.play();
-                let pause = document.querySelector(".pause")
-                pause.classList.toggle("sub-sin")
             } else {
                 vid.pause();
-                let pause = document.querySelector(".pause")
-                pause.classList.toggle("sub")
+
             }
     }
 
     function enableMute() {
             if (vid.muted == true) {
                 vid.muted = false;
-                let mute = document.querySelector(".mute")
-                mute.classList.toggle("sub-sin")
             } else {
                 vid.muted = true;
-                let mute = document.querySelector(".mute")
-                mute.classList.toggle("sub")
             }
     }
