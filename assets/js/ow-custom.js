7d99ccb065145e00179b25a89a6666417a85ec6f
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-07-22 16:00:10 -0500

    Iocns  and web changes

--- assets/js/ow-custom.js
+++ assets/js/ow-custom.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function ($){
 			autoplay:true,
 			dots:true,
 			autoplayTimeout:8500,
-			navText: ["<div class='nav-button owl-prev'><img class='img_after' src='./assets/img/arrow_left_ligth.png' style='width:25px'></div>", "<div class='nav-button owl-next'><img class='img_after' src='./assets/img/arrow_rigth_ligth.png' style='width:25px'></div>"],
+			navText: ["<div class='nav-button owl-prev'><img class='img_after' src='./assets/img/arrow_left_ligth.webp' style='width:25px'></div>", "<div class='nav-button owl-next'><img class='img_after' src='./assets/img/arrow_rigth_ligth.webp' style='width:25px'></div>"],
 			smartSpeed: 450,
 			responsive: {
 			       0 : {
